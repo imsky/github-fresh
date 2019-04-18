8606f3b9aa5621d054f278664074ebc58789cca9
Ivan Malopinsky <imsky@users.noreply.github.com>
2019-04-17 22:04:38 -0400

    move pull request listing out of main()

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 )
 
 // todo: get closed pull requests sorted by last updated time; stop processing after some cut-off (3h); get all unprotected branches - if any branches match those of the recently closed pull requests, delete them
@@ -12,7 +14,8 @@ import (
 // todo: Dockerfile, GitHub action
 
 type PullRequest struct {
-	Number uint64 `json:"number"`
+	Number   uint64    `json:"number"`
+	ClosedAt time.Time `json:"closed_at"`
 
 	PullRequestHead struct {
 		Ref string `json:"ref"`
@@ -23,13 +26,12 @@ type PullRequestResult struct {
 	PullRequests []PullRequest
 }
 
-func makeGithubAPIRequest(url string, method string) (res *http.Response, err error) {
+func makeGithubAPIRequest(url string, method string, token string) (res *http.Response, err error) {
 	client := &http.Client{}
 	req, err := http.NewRequest(method, url, nil)
 	if err != nil {
 		return nil, err
 	}
-	token := os.Getenv("GITHUB_TOKEN")
 	req.Header.Add("Authorization", "token "+token)
 	res, err = client.Do(req)
 	if err != nil {
@@ -38,13 +40,34 @@ func makeGithubAPIRequest(url string, method string) (res *http.Response, err er
 	return res, nil
 }
 
-func main() {
-	res, err := makeGithubAPIRequest("https://api.github.com/repos/OWNER/REPO/pulls?state=closed&sort=updated&direction=desc&per_page=100", "GET")
+func ListClosedPullRequests(user string, repo string, token string) []PullRequest {
+	pullRequests := make([]PullRequest, 0, 1)
+
+	res, err := makeGithubAPIRequest("https://api.github.com/repos/"+user+"/"+repo+"/pulls?state=closed&sort=updated&direction=desc&per_page=100", "GET", token)
+
 	if err != nil {
 		panic(err)
 	}
+
 	decoder := json.NewDecoder(res.Body)
-	var x PullRequestResult
-	err = decoder.Decode(&x.PullRequests)
-	fmt.Println(x)
+	var pullRequestResult PullRequestResult
+	err = decoder.Decode(&pullRequestResult.PullRequests)
+
+	if err != nil {
+		panic(err)
+	}
+
+	pullRequests = append(pullRequests, pullRequestResult.PullRequests...)
+
+	return pullRequests
+}
+
+func main() {
+	var token = flag.String("t", os.Getenv("GITHUB_TOKEN"), "GitHub API token")
+	var user = flag.String("u", "", "GitHub user")
+	var repo = flag.String("r", "", "GitHub repo")
+	//var days = flag.Int("d", 30, "Max age in days of checked pull requests")
+	flag.Parse()
+	fmt.Println(ListClosedPullRequests(*user, *repo, *token))
+	// u := url.URL{Host: "example.com", Path: "foo"}
 }
